b350f23e20a71d761cde0ff31c6652b5eaa53cc7
freyablue <76006917+freyablue@users.noreply.github.com>
2022-10-10 09:30:41 +0800

    Add files via upload

---
+++ src/main/java/edu/uchicago/gerber/_02arrays/E7_4.java
@@ -0,0 +1,50 @@
+package edu.uchicago.gerber._02arrays;
+import java.io.*;
+import java.util.*;
+public class E7_4 {
+    public static List<String> output=new ArrayList<String>();
+    public static void read_file(String filename){
+        BufferedReader b = null;
+
+        try{
+            b= new BufferedReader(new FileReader(filename));
+            while (b.readLine()!=null){
+                String l = b.readLine();
+                output.add(l);
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                b.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void output_file(String filename){
+        try {
+            File myObj = new File(filename);
+            if (myObj.createNewFile()) {
+                FileWriter myWriter = new FileWriter(filename);
+                for(int j = 0;j<output.size();j++){
+                    myWriter.write("/*"+String.valueOf(j+1)+"*/"+output.get(j));
+                }
+                myWriter.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        String f = "C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\test.txt";
+        read_file(f);
+        output_file("C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\output.txt");
+    }
+}
