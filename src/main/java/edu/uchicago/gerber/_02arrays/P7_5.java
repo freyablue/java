89592145605855248268b0f596b2e3f2aedd1589
freyablue <76006917+freyablue@users.noreply.github.com>
2022-10-10 12:30:57 +0800

    Update P7_5.java

--- src/main/java/edu/uchicago/gerber/_02arrays/P7_5.java
+++ src/main/java/edu/uchicago/gerber/_02arrays/P7_5.java
@@ -3,16 +3,16 @@ import java.io.*;
 import java.util.*;
 
 public class P7_5 {
-    public String[][] table;
 
-    public void read_file(String filename){
+    public static int i = 0;
+    public static String[][] table = new String[1000][1000];
+    public static void read_file(String filename){
         BufferedReader b = null;
-        int i = 0;
         try{
+            String l;
             b= new BufferedReader(new FileReader(filename));
-            while (b.readLine()!=null){
-                String l = b.readLine();
-                String[] store =l.split(",");
+            while ((l = b.readLine())!=null){
+                String[] store =l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                 table[i++] = store;
             }
         }catch(FileNotFoundException e){
@@ -27,13 +27,22 @@ public class P7_5 {
             }
         }
     }
-    public int numberOfRows(){
-        return table.length;
+    public static int numberOfRows(){
+        return i;
     }
-    public int numberOfFields(int row){
+    public static int numberOfFields(int row){
         return table[row].length;
     }
-    public String field(int row, int column){
+    public static String field(int row, int column){
         return table[row][column];
+    }
+
+    public static void main(String[] args) {
+//        String path = "C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\test_7_5";
+//        read_file(path);
+//        System.out.println(numberOfRows());
+//        System.out.println(numberOfFields(0));
+//        System.out.println(field(0,2));
+
     }
 }
