d3427b8bd75b693f76665d647da75993ff8faba3
freyablue <76006917+freyablue@users.noreply.github.com>
2022-10-10 09:30:20 +0800

    Add files via upload

---
+++ src/main/java/edu/uchicago/gerber/_02arrays/E6_16.java
@@ -0,0 +1,9 @@
+package edu.uchicago.gerber._02arrays;
+
+import java.util.Scanner;
+
+public class E6_16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
