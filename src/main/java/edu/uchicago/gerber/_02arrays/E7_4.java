1b18ffd1f8fe9b3e45c9ceb15329f0f191050069
freyablue <76006917+freyablue@users.noreply.github.com>
2022-10-10 11:22:03 +0800

    Update E7_4.java

--- src/main/java/edu/uchicago/gerber/_02arrays/E7_4.java
+++ src/main/java/edu/uchicago/gerber/_02arrays/E7_4.java
@@ -8,9 +8,11 @@ public class E7_4 {
 
         try{
             b= new BufferedReader(new FileReader(filename));
-            while (b.readLine()!=null){
-                String l = b.readLine();
+            String l;
+            while ((l=b.readLine())!=null){
+//                System.out.println(l);
                 output.add(l);
+
             }
         }catch(FileNotFoundException e){
             e.printStackTrace();
@@ -29,9 +31,10 @@ public class E7_4 {
         try {
             File myObj = new File(filename);
             if (myObj.createNewFile()) {
+                String newLine = System.getProperty("line.separator");
                 FileWriter myWriter = new FileWriter(filename);
                 for(int j = 0;j<output.size();j++){
-                    myWriter.write("/*"+String.valueOf(j+1)+"*/"+output.get(j));
+                    myWriter.write("/*"+String.valueOf(j+1)+"*/"+output.get(j)+newLine);
                 }
                 myWriter.close();
             }
@@ -45,6 +48,6 @@ public class E7_4 {
     public static void main(String[] args){
         String f = "C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\test.txt";
         read_file(f);
-        output_file("C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\output.txt");
+        //output_file("C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_02arrays\\output.txt");
     }
 }
