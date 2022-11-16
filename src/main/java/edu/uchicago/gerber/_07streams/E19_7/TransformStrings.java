afd36e09f1b20966de8c24353cb05e19dfe86993
freyablue <76006917+freyablue@users.noreply.github.com>
2022-11-16 15:12:41 +0800

    Update TransformStrings.java

--- src/main/java/edu/uchicago/gerber/_07streams/E19_7/TransformStrings.java
+++ src/main/java/edu/uchicago/gerber/_07streams/E19_7/TransformStrings.java
@@ -1,6 +1,12 @@
 package edu.uchicago.gerber._07streams.E19_7;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TransformStrings {
     //Write a lambda expression for a function that turns
@@ -13,10 +19,31 @@ public class TransformStrings {
         return s.charAt(0)+"..."+s.charAt(s.length()-1);
     }
 
-    public void filter(List<String> input){
-        for(String s:input){
+    public static void filter(String filename){
+        File f = new File(filename);
+        ArrayList<String> a = new ArrayList<>();
+        try(Scanner s = new Scanner(f)){
+            while(s.hasNext()){
+                String word = s.next();
+                a.add(word);
+            }
+            Stream<String> streams = a.stream();
+            List<String> result =streams.filter(str->str.length()>2).map(str->lambda(str)).collect(Collectors.toList());
+            for(String curr:result){
+                System.out.println(curr);
 
+            }
+
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Error Occurs: "+e.getMessage());
         }
 
+    }
+
+    public static void main(String[] args) {
+        String filename = "C:\\Users\\yijia chen\\Desktop\\projava-freyablue-master\\src\\main\\java\\edu\\uchicago\\gerber\\_07streams\\E19_7\\test";
+        filter(filename);
     }
 }
