9cf6fe5747ef1b308dc8eabacc007f5eba23ff0a
freyablue <76006917+freyablue@users.noreply.github.com>
2022-11-16 15:24:25 +0800

    Update CommaSeparated.java

--- src/main/java/edu/uchicago/gerber/_07streams/E19_5/CommaSeparated.java
+++ src/main/java/edu/uchicago/gerber/_07streams/E19_5/CommaSeparated.java
@@ -8,14 +8,13 @@ public class CommaSeparated {
     //public static <T> String toString(Stream<T> stream, int n)
     //that turns a Stream<T> into a comma-separated list of its first n elements.
     public static <T> String toString(Stream<T> stream, int n){
-        List<T> list = stream.limit(n).collect(Collectors.toList());
-        String output = "";
-        for(T ele:list){
-            output+=ele;
-        }
-        return output;
+        Stream<String> output= stream.limit(n).map(s->s.toString());
+        String result = output.collect(Collectors.joining(","));
+        return result;
     }
     public static void main(String[] args) {
+        Stream<Integer> nums = Stream.of(1,2,3,4,5,6,7,8,10,15,20);
+        System.out.println(toString(nums,5));
 
     }
 }
